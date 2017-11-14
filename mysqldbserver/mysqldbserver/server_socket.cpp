bb22f634aff5f571d6c7a05d7ade89c647da9f34
afei <648664838@qq.com>
2017-11-14 16:49:14 +0800

    network
    some code....

--- mysqldbserver/mysqldbserver/server_socket.cpp
+++ mysqldbserver/mysqldbserver/server_socket.cpp
@@ -1,28 +1,32 @@
-#include "server_socket.h"
+﻿#include "server_socket.h"
 #include <string>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <sys/epoll.h>
 using namespace std;
 
 namespace af
 {
-	void CMySocket::SetNonBlock()
+	//block socket
+	int SetNonBlock(int fd)
 	{
 		int flags = 0;
-		if ((flags = fcntl(mSocket, F_GETFL, 0)) < 0)
+		if ((flags = fcntl(fd, F_GETFL, 0)) < 0)
 		{
-			Close();
-			return;
+			return -1;
 		}
 		flags |= O_NONBLOCK;
-		if (fcntl(mSocket, F_SETFL, flags) < 0)
+		if (fcntl(fd, F_SETFL, flags) < 0)
 		{
-			Close();
-			return;
+			return -1;
 		}
-		return;
+		return 0;
 	}
+
 
 	CMySocket::CMySocket()
 	{
@@ -37,6 +41,9 @@ namespace af
 		strncpy(mIp, Ip, sizeof(mIp) - 1);
 	}
 
+	//create and bind socket
+	//if port is zero, random port;
+	//if ip is Null, INADDR_ANY
 	int CMySocket::InitSocket(int port, int protoType)
 	{
 		int ret = 0;
@@ -63,15 +70,9 @@ namespace af
 		mSocket = 0;
 	}
 
-	int CListenSocket::InitSocket(int port, int protoType)
-	{ 
-		int ret = CMySocket::InitSocket(port, protoType);
-		if (ret < 0)
-		{
-			return ret;
-		}
-
-		ret = listen(mSocket, 1024);
+	int CListenSocket::Listen()
+	{
+		int ret = listen(mSocket, 1024);
 		if (ret < 0)
 		{
 			return ret;
@@ -79,7 +80,11 @@ namespace af
 
 		if (mBlock == false)
 		{
-			SetNonBlock();
+			ret = SetNonBlock(mSocket);
+			if (ret < 0)
+			{
+				Close();
+			}
 		}
 		return ret;
 	}
@@ -104,12 +109,8 @@ namespace af
 		}
 
 		memset(pBuff, 0, sizeof(pBuff));
-		int nBuffNum = read(mSocket, pBuff, nLen);
-		if (nBuffNum <= 0)
-		{
-			Close();
-		}
-		return nBuffNum;
+
+		return read(mSocket, pBuff, nLen);
 	}
 
 	int CNetSocket::WriteData(char * pBuff, int nLen)
@@ -124,12 +125,7 @@ namespace af
 			return 0;
 		}
 
-		int nBuffNum = write(mSocket, pBuff, nLen);
-		if (nBuffNum <= 0)
-		{
-			Close();
-		}
-		return nBuffNum;
+		return write(mSocket, pBuff, nLen);
 	}
 
 	int CNetSocket::Connect(char * Ip, int port)
@@ -149,5 +145,127 @@ namespace af
 		return 0;
 	}
 
+	CMyMessage::CMyMessage()
+	{
+		mLen = 0;
+		memset(mBuff, 0, sizeof(mBuff));
+	}
 
+	int CMyMessage::ParseBuffToMessage(char * pBuff, int nLen)
+	{
+		mLen = nLen;
+		strncpy(mBuff, pBuff, sizeof(mBuff) - 1);
+	}
+	int CMyMessage::ParseMessageToBuff(char * pBuff, int& rLen)
+	{
+		memset(pBuff, 0, sizeof(pBuff));
+		strncpy(pBuff, mBuff, mLen);
+		rLen = mLen;
+	}
+
+	CMyEpoll::CMyEpoll(char * Ip, CDispatcher * pDisPatcher, bool bBlock = false) : mListenSocket(Ip, bBlock)
+	{
+		mEpollFd = 0;
+		mDispatcher = pDisPatcher;
+	}
+
+	int CMyEpoll::InitEpoll(int port, int protoType)
+	{
+		mEpollFd = epoll_create(MAX_SOCKET_NUM);
+
+		if (mEpollFd < 0)
+		{
+			return -1;
+		}
+
+		events_ = (struct epoll_event *)malloc(
+			sizeof(struct epoll_event) * MAX_SOCKET_NUM);
+
+		mListenSocket.InitSocket( port,  protoType);
+		mListenSocket.Listen();
+		return 0;
+	}
+
+	int CMyEpoll::RunEpoll(const int timeout)
+	{
+		int retval, numevents = 0;
+		retval = epoll_wait(mEpollFd, events_, MAX_SOCKET_NUM, timeout);
+		if (retval > 0)
+		{
+			for (int i = 0; i < retval; ++i)
+			{
+				if (events_[i].data.fd == mListenSocket.GetSocket())
+				{ 
+					int nFd = 0;
+					while ((nFd = mListenSocket.Accpet())> 0)
+					{
+						SetNonBlock(nFd);
+						AddEvent(nFd, EPOLLIN | EPOLLERR | EPOLLHUP);
+					}
+				}
+				else if (events_[i].events & EPOLLIN)
+				{
+					CNetSocket * pNetSocket = GetNetSocket(events_[i].data.fd);
+					if (pNetSocket == NULL)
+					{
+						DelEvent(events_[i].data.fd);
+						continue;
+					}
+					char pBuff[MAX_SOCKET_BUFF_SIZE];
+					int nLen = MAX_SOCKET_BUFF_SIZE;
+					int nBuffNum = pNetSocket->ReadData(pBuff,nLen);
+					if (nBuffNum <= 0)
+					{
+						DelEvent(events_[i].data.fd);
+						continue;
+					}
+					if (mDispatcher)
+					{
+						mDispatcher->OnRecvCharMessage(pBuff, nBuffNum);
+					}
+				}
+			}
+		}
+	}
+
+	CNetSocket * CMyEpoll::GetNetSocket(const int fd)
+	{
+		CNetSocketList::iterator it = mNetSocket.find(fd);
+		if (it == mNetSocket.end())
+		{
+			return NULL;
+		}
+
+		return  &(it->second);
+	}
+
+	int CMyEpoll::AddEvent(const int fd, const int event)
+	{
+		mNetSocket.insert(make_pair(fd,CNetSocket(fd))
+
+		struct epoll_event epollevent;
+		epollevent.data.fd = fd;
+		epollevent.events = event;
+		return epoll_ctl(mEpollFd, EPOLL_CTL_ADD, fd, &epollevent);
+	}
+	int CMyEpoll::DelEvent(const int fd)
+	{
+		CNetSocketList::iterator it = mNetSocket.find(fd);
+		if (it != mNetSocket.end())
+		{
+			it->second.Close();
+			mNetSocket.erase(fd);
+		}
+
+		struct epoll_event epollevent;
+		epollevent.data.fd = fd;
+		return epoll_ctl(mEpollFd, EPOLL_CTL_DEL, fd, &epollevent);
+	}
+	int CMyEpoll::ModEvent(const int fd, const int oldevent, const int newevent)
+	{
+		struct epoll_event epollevent;
+		epollevent.data.fd = fd;
+		epollevent.events = (oldevent | newevent);
+		return epoll_ctl(mEpollFd, EPOLL_CTL_MOD, fd, &epollevent);
+	}
 }
--- mysqldbserver/mysqldbserver/server_socket.h
+++ mysqldbserver/mysqldbserver/server_socket.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <map>
+
+using namespace std;
+
 namespace af
 {
+	enum
+	{
+		MAX_SOCKET_BUFF_SIZE = 64000,
+	};
+	int SetNonBlock(int fd);
 	class CMySocket
 	{
 	public:
@@ -11,17 +20,15 @@ namespace af
 		};
 	public:
 		CMySocket();
-		explicit CMySocket(char * Ip, bool bBlock == false);
+		explicit CMySocket(char * Ip, bool bBlock = false);
 		virtual ~CMySocket();
-		virtual int InitSocket(int port, int protoType == SOCK_STREAM);
+		virtual int InitSocket(int port, int protoType = SOCK_STREAM);
 		virtual void Close();
 
 	public:
 		int GetSocket(){ return mSocket; }
 		void SetSocket(int socket){ mSocket = socket; }
 	protected:
-		void SetNonBlock();
-	private:
 		char   mIp[ENUM_IP_STR_LENGTH];
 		int    mPort;
 		bool   mBlock;
@@ -30,20 +37,25 @@ namespace af
 	class CListenSocket : public CMySocket
 	{
 	public:
-		using CMySocket::CMySocket;
+		CListenSocket() :CMySocket(){}
+		explicit CListenSocket(char * Ip, bool bBlock = false) :CMySocket(Ip,bBlock){}
 
-		int InitSocket(int port, int protoType == SOCK_STREAM);
+		int Listen();
 		int Accpet();
 	};
 
 	class CNetSocket : public CMySocket
 	{
 	public:
-		using CMySocket;
+		CNetSocket() :CMySocket(){}
+		explicit CNetSocket(char * Ip, bool bBlock = false) :CMySocket(Ip, bBlock){}
+		CNetSocket(int socket) :CMySocket(){ mSocket = socket;};
 		int Connect(char * Ip, int port);
 		int ReadData(char * pBuff, int nLen);
 		int WriteData(char * pBuff, int nLen);
 	};
+
+	typedef map<int, CNetSocket> CNetSocketList;
 
 	class CMyMessage
 	{
@@ -56,12 +68,47 @@ namespace af
 		CMyMessage();
 
 	public:
-	   virtual	int ParseMessageToBuff(char * pBuff, int nLen);
+	   virtual	int ParseMessageToBuff(char * pBuff, int& rLen);
 	   virtual	int ParseBuffToMessage(char * pBuff, int nLen);
 	public:
+		int mLen;
 		char mBuff[MAX_SOCKET_BUFF_SIZE];
 	};
 
-	//class C
+	typedef map<int, CNetSocket> CNetSocketList;
 
+	class CDispatcher
+	{
+	public:
+		void OnRecvCharMessage(char * pBuff, int nLen);
+
+	};
+
+	class CMyEpoll
+	{
+	public:
+		enum
+		{
+			MAX_SOCKET_NUM = 1024,
+		};
+
+	public:
+		CMyEpoll(char * Ip, CDispatcher * pDisPatcher, bool bBlock = false);
+		~CMyEpoll();
+		int InitEpoll(int port, int protoType = SOCK_STREAM);
+		int RunEpoll(const int timeout);
+		int AddEvent(const int fd, const int event);
+		int DelEvent(const int fd);
+		int ModEvent(const int fd, const int oldevent, const int newevent);
+
+
+	public:
+		CNetSocket * GetNetSocket(const int fd);
+	public:
+		struct epoll_event *events_;
+		CNetSocketList mNetSocket;
+		int mEpollFd;
+		CListenSocket mListenSocket;
+		CDispatcher * mDispatcher;
+	};
 }
